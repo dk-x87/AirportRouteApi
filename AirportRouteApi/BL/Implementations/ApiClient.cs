fe9c8cc97e96df007380b53d65b7305be986bdc3
dk-x87 <45285148+dk-x87@users.noreply.github.com>
2021-11-29 23:21:07 +0300

    - fix

--- AirportRouteApi/BL/Implementations/ApiClient.cs
+++ AirportRouteApi/BL/Implementations/ApiClient.cs
@@ -16,6 +16,8 @@ namespace AirportRouteApi.BL.Implementations
 
         public async Task<List<Route>> GetRoutesByAirports(string from, string to, int maxTransferCount, CancellationToken ct)
         {
+            if (ct.IsCancellationRequested)
+                return null;
             var routes = await TrySearchAsync(from, to, maxTransferCount, ct);
             return routes.Count > 0 && routes[0].SrcAirport == from && routes[routes.Count - 1].DestAirport == to ? routes : null;
         }
@@ -29,6 +31,8 @@ namespace AirportRouteApi.BL.Implementations
 
         private async Task<bool> IsActiveAirline(string alias, CancellationToken ct)
         {
+            if (ct.IsCancellationRequested)
+                return false;
             var airlines = await sender.GetDataForAirline(alias, ct);
             return airlines != null && airlines.Count > 0 && airlines.Find(x => x.Active) != null;
         }
@@ -36,36 +40,41 @@ namespace AirportRouteApi.BL.Implementations
         private async Task<List<Route>> TrySearchAsync(string from, string to, int maxTransferCount, CancellationToken ct)
         {
             List<Route> routes = new List<Route>();
-            var routesFromSource = await sender.GetDataForRoute(from, ct);
-            var sourceToDest = routesFromSource.Find(x => x.SrcAirport == from && x.DestAirport == to);
-            if (sourceToDest != null
-                && await IsValidAirport(sourceToDest.DestAirport, ct)
-                && await IsActiveAirline(sourceToDest.Airline, ct))
+
+            if (!ct.IsCancellationRequested)
             {
-                routes.Add(sourceToDest);
-            }
-            else
-            {
-                if (routes.Count < maxTransferCount)
+                var routesFromSource = await sender.GetDataForRoute(from, ct);
+                var sourceToDest = routesFromSource.Find(x => x.SrcAirport == from && x.DestAirport == to);
+                if (sourceToDest != null
+                    && await IsValidAirport(sourceToDest.DestAirport, ct)
+                    && await IsActiveAirline(sourceToDest.Airline, ct))
                 {
-                    bool detected = false;
-                    int count = 0;
-                    while (count <= routesFromSource.Count - 1 && !detected)
+                    routes.Add(sourceToDest);
+                }
+                else
+                {
+                    if (routes.Count < maxTransferCount)
                     {
-                        if (await IsValidAirport(routesFromSource[count].DestAirport, ct) && await IsActiveAirline(routesFromSource[count].Airline, ct))
+                        bool detected = false;
+                        int count = 0;
+                        while (count <= routesFromSource.Count - 1 && !detected)
                         {
-                            var tryRoutes = await TrySearchAsync(routesFromSource[count].DestAirport, to, maxTransferCount - 1, ct);
-                            if (tryRoutes.Count > 0)
+                            if (await IsValidAirport(routesFromSource[count].DestAirport, ct) && await IsActiveAirline(routesFromSource[count].Airline, ct))
                             {
-                                routes.Add(routesFromSource[count]);
-                                routes.AddRange(tryRoutes);
-                                detected = true;
+                                var tryRoutes = await TrySearchAsync(routesFromSource[count].DestAirport, to, maxTransferCount - 1, ct);
+                                if (tryRoutes.Count > 0)
+                                {
+                                    routes.Add(routesFromSource[count]);
+                                    routes.AddRange(tryRoutes);
+                                    detected = true;
+                                }
                             }
+                            count++;
                         }
-                        count++;
                     }
                 }
             }
+            
             return routes;
         }
 
